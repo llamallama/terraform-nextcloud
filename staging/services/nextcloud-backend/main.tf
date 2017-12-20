31d0f874402222c92894b7fd1670864f876d0846
Chris Jones <chris@sysadminchris.com>
2017-12-19 19:44:33 -0500

    Backend now uses AWS TLS cert

--- staging/services/nextcloud-backend/main.tf
+++ staging/services/nextcloud-backend/main.tf
@@ -14,10 +14,13 @@ data "terraform_remote_state" "vpc" {
 }
 
 module "lb" {
-  #source = "git::git@github.com:llamallama/terraform-modules.git//lb?ref=v0.0.3"
-  source = "../../../../terraform-modules/lb"
+  source = "git::git@github.com:llamallama/terraform-modules.git//lb?ref=v0.0.6"
+  #source = "../../../../terraform-modules/lb"
 
   lb_name = "NextcloudStaging"
+  use_tls = 1
+  certificate_arn = "arn:aws:acm:us-east-1:887110813782:certificate/8906e621-ee45-40b6-9ba7-7e31017d80ce"
   subnets = ["${data.terraform_remote_state.vpc.public_subnet_ids}"]
   vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
+  security_groups = "${data.terraform_remote_state.vpc.default_security_group_id}"
 }
