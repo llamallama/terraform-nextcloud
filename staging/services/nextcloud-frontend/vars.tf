6ce552e70c7522feccaddca1a9221ccaf09d621c
Chris Jones <chris@sysadminchris.com>
2017-12-19 19:44:58 -0500

    Moving nextcloud frontend variables to their own file

---
+++ staging/services/nextcloud-frontend/vars.tf
@@ -0,0 +1,9 @@
+variable "nextcloud_url" {
+  default = "https://download.nextcloud.com/server/releases/nextcloud-12.0.3.tar.bz2"
+}
+variable "domain_name" {
+  default = "staging-cloud.piptogrep.org"
+}
+variable "access_ip" {
+  default = "142.196.165.134/32"
+}
