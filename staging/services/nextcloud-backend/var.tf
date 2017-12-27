c9494b02173a1e70dc8cf83a64afcf8fc2343eca
Chris Jones <chris@sysadminchris.com>
2017-12-27 07:17:09 -0500

    Adding variables

---
+++ staging/services/nextcloud-backend/var.tf
@@ -0,0 +1,9 @@
+  variable "lb_name" {
+    default = "NextcloudStaging"
+  }
+  variable "use_tls" {
+    default = 1
+  }
+  variable "certificate_arn" {
+    default = "arn:aws:acm:us-east-1:887110813782:certificate/8906e621-ee45-40b6-9ba7-7e31017d80ce"
+  }
---
+++ staging/vpc/vars.tf
@@ -0,0 +1,15 @@
+variable "environment_name" {
+  default = "Staging"
+}
+variable "vpc_cidr" {
+  default = "172.16.0.0/16"
+}
+variable "public_1a_cidr" {
+  default = "172.16.0.0/24"
+}
+variable "public_1b_cidr" {
+  default = "172.16.1.0/24"
+}
+variable "public_1c_cidr" {
+  default = "172.16.2.0/24"
+}
