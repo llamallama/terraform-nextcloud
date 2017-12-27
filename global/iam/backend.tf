c14b23f7adce5dda75a74225e779e090f40ef4f8
Chris Jones <chris@sysadminchris.com>
2017-12-27 07:15:27 -0500

    Switching IAM to global folder

---
+++ global/iam/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    profile = "chris"
+    bucket = "chris-terraform-states"
+    key = "nextcloud/global/iam/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
