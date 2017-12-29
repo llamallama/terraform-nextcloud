5f2f7863d62628e4ef48c57b72cb0366f261dee2
Chris Jones <chris@sysadminchris.com>
2017-12-28 21:00:42 -0500

    Adding IAM users

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -3,19 +3,19 @@ provider "aws" {
  profile = "chris"
 }
 
-module "ec2Role" {
+module "staging_ec2_role" {
   source = "git::git@github.com:llamallama/terraform-modules.git//iam//role?ref=v0.0.6"
   #source = "../../../terraform-modules/iam/role"
   effect = "Allow"
-  name = "NextcloudStagingEC2Role"
+  name = "NextcloudStagingEc2Role"
   service = "ec2.amazonaws.com"
 }
 
-module "ec2RolePolicy" {
+module "staging_ec2_role_policy" {
   source = "git::git@github.com:llamallama/terraform-modules.git//iam//role-policy?ref=v0.0.6"
   #source = "../../../terraform-modules/iam/role-policy"
-  name = "NextcloudStagingEC2RolePolicy"
-  role = "${module.ec2Role.id}"
+  name = "NextcloudStagingEc2RolePolicy"
+  role = "${module.staging_ec2_role.id}"
   effect = "Allow"
   resource = "arn:aws:s3:::cloud.pipetogrep.org/staging/*"
   action = <<EOF
@@ -23,7 +23,106 @@ module "ec2RolePolicy" {
 EOF
 }
 
-resource "aws_iam_instance_profile" "instance_profile" {
+resource "aws_iam_instance_profile" "staging_instance_profile" {
   name  = "NextcloudStagingInstanceProfile"
-  role = "${module.ec2Role.name}"
+  role = "${module.staging_ec2_role.name}"
+}
+
+resource "aws_iam_user" "nextcloud_staging" {
+  name = "NextcloudStaging"
+}
+
+resource "aws_iam_user_policy" "s3_nextcloud_staging" {
+  name = "S3NextcloudStaging"
+  user = "${aws_iam_user.nextcloud_staging.name}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::staging-cloud.pipetogrep.org"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:DeleteObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::staging-cloud.pipetogrep.org/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+module "prod_ec2_role" {
+  source = "git::git@github.com:llamallama/terraform-modules.git//iam//role?ref=v0.0.6"
+  #source = "../../../terraform-modules/iam/role"
+  effect = "Allow"
+  name = "NextcloudProdEc2Role"
+  service = "ec2.amazonaws.com"
+}
+
+module "prod_ec2_role_policy" {
+  source = "git::git@github.com:llamallama/terraform-modules.git//iam//role-policy?ref=v0.0.6"
+  #source = "../../../terraform-modules/iam/role-policy"
+  name = "NextcloudProdEc2RolePolicy"
+  role = "${module.prod_ec2_role.id}"
+  effect = "Allow"
+  resource = "arn:aws:s3:::cloud.pipetogrep.org/prod/*"
+  action = <<EOF
+"s3:GetObject"
+EOF
+}
+
+resource "aws_iam_instance_profile" "prod_instance_profile" {
+  name  = "NextcloudProdInstanceProfile"
+  role = "${module.prod_ec2_role.name}"
+}
+
+resource "aws_iam_user" "nextcloud_prod" {
+  name = "NextcloudProd"
+}
+
+resource "aws_iam_user_policy" "s3_nextcloud_prod" {
+  name = "S3NextcloudProd"
+  user = "${aws_iam_user.nextcloud_prod.name}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::prod-cloud.pipetogrep.org"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:DeleteObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::prod-cloud.pipetogrep.org/*"
+            ]
+        }
+    ]
+}
+EOF
 }
--- global/iam/outputs.tf
+++ global/iam/outputs.tf
@@ -1,3 +1,6 @@
-output "iam_instance_profile" {
-  value = "${aws_iam_instance_profile.instance_profile.name}"
+output "staging_iam_instance_profile" {
+  value = "${aws_iam_instance_profile.staging_instance_profile.name}"
+}
+output "prod_iam_instance_profile" {
+  value = "${aws_iam_instance_profile.prod_instance_profile.name}"
 }
